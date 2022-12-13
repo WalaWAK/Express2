db172adadda4a302a9032f83479f307627baa70e
Myles <mylesmorales012@gmail.com>
2022-12-13 03:29:09 -0600

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.listen(port, function () {
+    console.log("listening")
+})
+
 app.get('/greeting/:name'), (req, res) => {
 	console.log(req.params);
 	res.send('Hey ' + req.params.name);
-};
+}
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+	console.log(req.params);
+	res.send(`Total: ${req.params.total}   Tip: ${req.params.total * (req.params.tipPercentage / 100)}`)
+})
